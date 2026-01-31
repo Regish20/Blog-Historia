ad508ab2552380ee22a198e52d3531826c2b9766
Regish20 <regis.h2704@gmail.com>
2026-01-30 22:49:06 -0500

    Añadiendo Icono Pantalla

--- includes/header.php
+++ includes/header.php
@@ -21,6 +21,8 @@ require_once 'markdown.php';
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!-- CSS Personalizado -->
     <link rel="stylesheet" href="css/estilos.css">
+    <!-- Favicon -->
+    <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='%23d4a574'><path d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z'/><path d='M9 16.17l-4.17-4.17L3.83 13l5.17 5.17L21 6.83 19.59 5.41z'/><path d='M11 7h2v6h-2z'/><path d='M7 9v2h6V9z'/></svg>">
 </head>
 <body>
     <?php
